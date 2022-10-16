d5f87b02b96a371175f05ea122f7f610056d9a3d
Japheth Namukuru <japhethnamukuru@gmail.com>
2022-10-16 15:36:47 +0300

    updated files

--- denis/website/urls.py
+++ denis/website/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 
+app_name = 'website'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('health/', views.health, name='health'),
+    path('fitness/', views.fitness, name='fitness'),
+    path('marketing/', views.marketing, name='marketing'),
+    path('mentorship/', views.mentorship, name='mentorship'),
 ]
--- denis/website/views.py
+++ denis/website/views.py
@@ -5,3 +5,15 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'website/index.html')
+
+def health(request):
+    return render(request, 'website/health.html')
+
+def fitness(request):
+    return render(request, 'website/fitness.html')
+
+def marketing(request):
+    return render(request, 'website/marketing.html')
+
+def mentorship(request):
+    return render(request, 'website/mentorship.html')
